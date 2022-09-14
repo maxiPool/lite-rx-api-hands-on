209374e5b50e087724986ef76894ed411260bac3
maxiPool <84409780+maxiPool@users.noreply.github.com>
2022-09-14 13:19:14 -0400

    feat: solve problems (#1)
    * chore: styling & ignore
    
    * feat: done

--- src/main/java/io/pivotal/literx/Part02Mono.java
+++ src/main/java/io/pivotal/literx/Part02Mono.java
@@ -2,6 +2,8 @@ package io.pivotal.literx;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 /**
  * Learn how to create Mono instances.
  *
@@ -12,30 +14,30 @@ public class Part02Mono {
 
 //========================================================================================
 
-	// TODO Return an empty Mono
-	Mono<String> emptyMono() {
-		return null;
-	}
+  // TODO Return an empty Mono
+  Mono<String> emptyMono() {
+    return Mono.empty();
+  }
 
 //========================================================================================
 
-	// TODO Return a Mono that never emits any signal
-	Mono<String> monoWithNoSignal() {
-		return null;
-	}
+  // TODO Return a Mono that never emits any signal
+  Mono<String> monoWithNoSignal() {
+    return Mono.never();
+  }
 
 //========================================================================================
 
-	// TODO Return a Mono that contains a "foo" value
-	Mono<String> fooMono() {
-		return null;
-	}
+  // TODO Return a Mono that contains a "foo" value
+  Mono<String> fooMono() {
+    return Mono.just("foo");
+  }
 
 //========================================================================================
 
-	// TODO Create a Mono that emits an IllegalStateException
-	Mono<String> errorMono() {
-		return null;
-	}
+  // TODO Create a Mono that emits an IllegalStateException
+  Mono<String> errorMono() {
+    return Mono.error(IllegalStateException::new);
+  }
 
 }
--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -6,6 +6,9 @@ import io.pivotal.literx.repository.ReactiveUserRepository;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import static io.pivotal.literx.domain.User.JESSE;
+import static io.pivotal.literx.domain.User.SKYLER;
+
 /**
  * Learn how to control the demand.
  *
@@ -13,34 +16,49 @@ import reactor.test.StepVerifier;
  */
 public class Part06Request {
 
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
+  ReactiveRepository<User> repository = new ReactiveUserRepository();
 
 //========================================================================================
 
-	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
-	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
-	}
+  // TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
+  StepVerifier requestAllExpectFour(Flux<User> flux) {
+    return StepVerifier
+        .create(flux)
+        .expectNextCount(4L)
+        .expectComplete();
+  }
 
 //========================================================================================
 
-	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE then stops verifying by cancelling the source
-	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
-		return null;
-	}
+  // TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER
+  //  then requests another value and expects User.JESSE then stops verifying by cancelling the source
+  StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
+    return StepVerifier
+        .create(flux)
+        .thenRequest(1)
+        .expectNext(SKYLER)
+        .thenRequest(1)
+        .expectNext(JESSE)
+        .thenCancel();
+  }
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
-	Flux<User> fluxWithLog() {
-		return null;
-	}
+  // TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
+  Flux<User> fluxWithLog() {
+    return repository.findAll()
+        .log();
+  }
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" at first, "firstname lastname" for all values and "The end!" on complete
-	Flux<User> fluxWithDoOnPrintln() {
-		return null;
-	}
+  // TODO Return a Flux with all users stored in the repository that prints "Starring:" at first,
+  //  "firstname lastname" for all values and "The end!" on complete
+  Flux<User> fluxWithDoOnPrintln() {
+    return repository.findAll()
+        .doOnSubscribe(u -> System.out.println("Starring:"))
+        .doOnNext(u -> System.out.printf("%s %s%n", u.getFirstname(), u.getLastname()))
+        .doOnComplete(() -> System.out.println("The end!"));
+  }
 
 }
